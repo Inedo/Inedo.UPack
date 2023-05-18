a22afde3bad015eeb7289d9ebd009634fe489e70
Wouter de Vries <wouterdevries@silverfit.nl>
2023-05-18 11:08:55 +0200

    Add unit tests for JsonConverters

---
+++ src/Inedo.UPack.Tests/Json/DependencyJsonConverterTests.cs
@@ -0,0 +1,59 @@
+﻿using Inedo.UPack.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+
+namespace Inedo.UPack.Tests.Json
+{
+    [TestClass]
+    public class DependencyJsonConverterTests
+    {
+        private readonly JsonSerializerOptions _options = new()
+        {
+            Converters =
+            {
+                new UniversalPackageDependencyJsonConverter(),
+            }
+        };
+
+        [TestMethod]
+        public void TestDependency()
+        {
+            List<UniversalPackageDependency> expectedDependencies = new()
+            {
+                new(null, "myname", UniversalPackageVersionRange.Any),
+                new("mygroup", "myname", UniversalPackageVersionRange.Any),
+                new(null, "myname", new UniversalPackageVersion(1, 2, 3)),
+                new(null, "myname", new UniversalPackageVersionRange(new(3,0,0), false, null, false)),
+                null,
+            };
+            var json = @"[
+                ""myname"",
+                ""mygroup/myname"",
+                ""myname:1.2.3"",
+                ""myname:[3.0.0,]"",
+                null
+            ]";
+            var parsed = JsonSerializer.Deserialize<List<UniversalPackageDependency>>(json, _options);
+
+            CollectionAssert.AreEqual(expectedDependencies, parsed);
+        }
+
+        [TestMethod]
+        [DataRow("mygroup", "myname", null)]
+        [DataRow("mygroup", "myname", "*")]
+        [DataRow("mygroup", "myname", "3.0.1")]
+        [DataRow("mygroup", "myname", "[3.0.0,]")]
+        [DataRow("mygroup", "myname", "[1.0.0,2.0.0)")]
+        public void TestDependencyReversible(string group, string name, string versionRange)
+        {
+            var vrange = versionRange is null ? null : UniversalPackageVersionRange.TryParse(versionRange, out var vr) ? vr : null;
+
+            UniversalPackageDependency id = new(group, name, vrange);
+
+            var json = JsonSerializer.Serialize(id, _options);
+            var parsed = JsonSerializer.Deserialize<UniversalPackageDependency>(json, _options);
+
+            Assert.AreEqual(id, parsed);
+        }
+    }
+}
---
+++ src/Inedo.UPack.Tests/Json/VersionRangeJsonConverterTests.cs
@@ -0,0 +1,62 @@
+﻿using Inedo.UPack.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+
+namespace Inedo.UPack.Tests.Json
+{
+    [TestClass]
+    public class VersionRangeJsonConverterTests
+    {
+        private readonly JsonSerializerOptions _options = new()
+        {
+            Converters =
+            {
+                new UniversalPackageVersionJsonConverter(),
+                new UniversalPackageVersionRangeJsonConverter(),
+            }
+        };
+
+        [TestMethod]
+        public void TestVersionRange()
+        {
+            List<UniversalPackageVersionRange> expectedVersions = new()
+            {
+                UniversalPackageVersionRange.Any,
+                new UniversalPackageVersionRange(new(1, 2, 3)),
+                new UniversalPackageVersionRange(new(1, 0, 0), false, new(2, 0, 0), true),
+                new UniversalPackageVersionRange(new(3, 0, 0), false, null, true),
+                UniversalPackageVersionRange.Any,
+            };
+            var json = @"[
+                ""*"",
+                ""1.2.3"",
+                ""[1.0.0,2.0.0)"",
+                ""[3.0.0,]"",
+                null
+            ]";
+            var parsed = JsonSerializer.Deserialize<List<UniversalPackageVersionRange>>(json, _options);
+
+            CollectionAssert.AreEqual(expectedVersions, parsed);
+        }
+
+        [TestMethod]
+        [DataRow("1.2.3", false, null, false)]
+        [DataRow("1.0.0", false, "2.0.0", true)]
+        public void TestVersionRangeReversible(string lowerBound, bool lowerExclusive, string upperBound, bool upperExclusive)
+        {
+            UniversalPackageVersion lb = parseOrDefault(lowerBound);
+            UniversalPackageVersion ub = parseOrDefault(upperBound);
+            UniversalPackageVersionRange version = new(lb, lowerExclusive, ub, upperExclusive);
+
+            var json = JsonSerializer.Serialize(version, _options);
+            var parsed = JsonSerializer.Deserialize<UniversalPackageVersionRange>(json, _options);
+
+            Assert.AreEqual(version, parsed);
+
+            static UniversalPackageVersion parseOrDefault(string value)
+            {
+                return value is null ? null : UniversalPackageVersion.Parse(value);
+            }
+        }
+    }
+}
