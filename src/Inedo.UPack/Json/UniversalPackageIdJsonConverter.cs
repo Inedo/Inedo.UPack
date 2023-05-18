6eff3420c7c2fb37934ab13bd8e66f8ad36c0be5
Wouter de Vries <wouterdevries@silverfit.nl>
2023-05-18 10:53:29 +0200

    Add JsonConverters

---
+++ src/Inedo.UPack/Json/UniversalPackageIdJsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Inedo.UPack.Json
+{
+    public class UniversalPackageIdJsonConverter : JsonConverter<UniversalPackageId>
+    {
+        public override UniversalPackageId? Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return value == null
+                ? default
+                : UniversalPackageId.Parse(value);
+        }
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            UniversalPackageId value,
+            JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
---
+++ src/Inedo.UPack/Json/UniversalPackageVersionRangeJsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Inedo.UPack.Json
+{
+    public class UniversalPackageVersionRangeJsonConverter : JsonConverter<UniversalPackageVersionRange>
+    {
+        public override UniversalPackageVersionRange Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return value == null
+                ? default
+                : UniversalPackageVersionRange.Parse(value);
+        }
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            UniversalPackageVersionRange value,
+            JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
